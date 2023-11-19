2b11bf62a2cc960a1509e8e6a10bf1a160b8c286
GavinOP <HaoxiangZhang@link.cuhk.edu.hk>
2023-11-19 16:04:42 +0800

    class object
    - finish class object to calculate iv
    - next step is to fit the iv curve

---
+++ Code/data_cleaning.py
@@ -0,0 +1,142 @@
+# import necessary libraries
+import pandas as pd
+import yfinance as yf
+import warnings
+import os
+
+
+# define it a class for data cleaning
+class data_cleaning:
+    def __init__(self, option_data):
+        self.option_data = option_data
+        # self.expiry_list = None  # Initialize expiry_list as None
+
+    def format_data(self):
+        # ignore SettingWithCopyWarning:
+        pd.options.mode.chained_assignment = None
+        
+        # drop na rows
+        self.option_data = self.option_data.dropna()
+        
+        # ticker breakdown for call
+        self.option_data[['Index', 'Expiry', 'strike']] = self.option_data['Ticker'].str.split(' ', expand=True)
+        self.option_data['Type'] = self.option_data['strike'].str[0]
+        self.option_data.drop(['strike'], axis=1, inplace=True)
+
+        # ticker breakdown for put
+        self.option_data[['Index.1', 'Expiry.1', 'strike']] = self.option_data['Ticker.1'].str.split(' ', expand=True)
+        self.option_data['Type.1'] = self.option_data['strike'].str[0]
+        self.option_data.drop(['strike'], axis=1, inplace=True)
+
+        # spit call and put data
+        call_data = self.option_data[self.option_data.columns[~self.option_data.columns.str.contains('.1')]]
+        put_data = self.option_data[self.option_data.columns[self.option_data.columns.str.contains('.1')]]
+        put_data.columns = call_data.columns
+        call_data = call_data[['Ticker', 'Index', 'Type', 'Expiry', 'Strike', 'Bid', 'Ask', 'Last', 'Volm', 'IVM']]
+        put_data = put_data[['Ticker', 'Index', 'Type', 'Expiry', 'Strike', 'Bid', 'Ask', 'Last', 'Volm', 'IVM']]
+
+        # redefine option data as call concat put, convert strike to integer, convert expiry to string format, sort data by expiray and strike, delete 0 volume rows 
+        self.option_data = pd.concat([call_data, put_data], axis=0)
+        self.option_data['Strike'] = self.option_data['Strike'].astype(int)
+        self.option_data['Expiry'] = pd.to_datetime(self.option_data['Expiry'], format='%m/%d/%y')
+        self.option_data['Expiry'] = self.option_data['Expiry'].dt.date
+        self.option_data = self.option_data.sort_values(by=['Type', 'Expiry', 'Index', 'Strike'])
+        self.option_data['Expiry'] = self.option_data['Expiry'].astype(str)
+        self.option_data = self.option_data[self.option_data['Volm'] != 0]
+
+        # only use SPX data
+        self.option_data = self.option_data[self.option_data['Index'] == 'SPX']
+
+        # set index as number
+        self.option_data = self.option_data.reset_index(drop=True)
+
+        return self.option_data
+
+
+    def check_iv_number(self):
+        # keep expiry with enough implied volatility data
+        self.expiry_list = self.option_data['Expiry'].unique()
+        expiry_keep = []
+
+        for expiry in self.expiry_list:
+            # for each expiry, get the unique strike
+            option_data_expiry = self.option_data[self.option_data['Expiry'] == expiry]
+            option_strike = option_data_expiry['Strike'].unique()
+
+            # if strike_keep has more than 10 unique strike, keep the expiry
+            if len(option_strike) > 10:
+                expiry_keep.append(expiry)
+
+        # keep the expiry with enough implied volatility data
+        self.option_data = self.option_data[self.option_data['Expiry'].isin(expiry_keep)]
+
+        return self.option_data
+    
+
+    def get_spx_hist(self, start, end):
+        spx = yf.Ticker('^GSPC')
+        spx_hist = spx.history(start=start, end=end)
+        spx_hist = spx_hist.reset_index()
+        spx_hist = spx_hist[['Date', 'Open']]
+        spx_hist['Date'] = pd.to_datetime(spx_hist['Date'], format='%Y-%m-%d')
+        spx_hist['Date'] = spx_hist['Date'].dt.date
+        spx_hist['Date'] = spx_hist['Date'].astype(str)
+        spx_hist = spx_hist.rename(columns={'Close':'SPX'})
+        spx_hist = spx_hist.set_index('Date')
+        # check if the file exists
+        if not os.path.exists(f'./Public/data/index_price/spx_hist_{start}_{end}.csv'):
+            spx_hist.to_csv(f'./Public/data/index_price/spx_hist_{start}_{end}.csv')
+        
+        return spx_hist
+    
+    
+    def extract_option_price(self):
+        # ignore warning
+        warnings.simplefilter(action='ignore', category=FutureWarning)
+
+        # store call price and put price with same strike and expiry in the same row of a dataframe
+        option_price = pd.DataFrame(columns=['Strike', 'Expiry', 'c', 'p'])
+        option_data_spx = self.option_data[self.option_data['Index'] == 'SPX']
+        expiry_list = option_data_spx['Expiry'].unique()
+        
+        for expiry in expiry_list:
+            # for each expiry
+            option_data_expiry = self.option_data[self.option_data['Expiry'] == expiry]
+            option_strike = option_data_expiry['Strike'].unique()
+
+            for strike in option_strike:
+                # for each strike
+                option_data_strike = option_data_expiry[option_data_expiry['Strike'] == strike]
+                
+                # if call and put price exist, store the price in the dataframe
+                # call_price = option_data_strike[option_data_strike['Type'] == 'C']['Last']
+                # put_price = option_data_strike[option_data_strike['Type'] == 'P']['Last']
+                # put price and call price are the mid price of bid and ask
+                call_price = option_data_strike[option_data_strike['Type'] == 'C'][['Bid', 'Ask']].mean(axis=1)
+                put_price = option_data_strike[option_data_strike['Type'] == 'P'][['Bid', 'Ask']].mean(axis=1)
+
+                if not call_price.empty:
+                    call_price = call_price.values[0]
+                if not put_price.empty:
+                    put_price = put_price.values[0]
+
+                temp_price = pd.DataFrame({'Strike': strike, 'Expiry': expiry, 'c': call_price, 'p': put_price}, index=[0])
+                option_price = pd.concat([option_price, temp_price], axis=0)
+        
+        # set index as number
+        option_price = option_price.reset_index(drop=True)
+
+        return option_price
+    
+    
+
+
+    
+    
+# -------------------------- test code --------------------------
+if __name__ == '__main__':
+    raw_data = pd.read_csv('./Public/data/option_price/20230901/spx_option_0901.csv')
+    option_data = data_cleaning(raw_data).format_data()
+    print(option_data)
+    # spx_hist = get_spx_hist('2021-08-01', '2021-09-01')
+    # print(spx_hist)
---
+++ Code/implied_vol.py
@@ -0,0 +1,258 @@
+# import necessary libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from scipy.stats import norm
+from functools import partial
+
+
+# define BS model
+def BS_fwd_pricer(F, K, T, sig, r, isCall):
+    d1 = (np.log(F / K) + 0.5 * sig ** 2 * T) / (sig * np.sqrt(T))
+    d2 = d1 - sig * np.sqrt(T)
+    
+    if isCall:
+        option_price = np.exp(-r * T) * (F * norm.cdf(d1) - K * norm.cdf(d2))
+    else:
+        option_price = np.exp(-r * T) * (K * norm.cdf(-d2) - F * norm.cdf(-d1))
+
+    return option_price
+
+
+# use bisection method to find implied volatility
+def bisection_implied_vol(F, K, T, r, option_price, tol=0.05, isCall=True):
+    # define BS model
+    def BS_fwd_pricer(F, K, T, sig, r, isCall):
+        d1 = (np.log(F / K) + 0.5 * sig ** 2 * T) / (sig * np.sqrt(T))
+        d2 = d1 - sig * np.sqrt(T)
+        
+        if isCall:
+            option_price = np.exp(-r * T) * (F * norm.cdf(d1) - K * norm.cdf(d2))
+        else:
+            option_price = np.exp(-r * T) * (K * norm.cdf(-d2) - F * norm.cdf(-d1))
+
+        return option_price
+    
+    # leave the isCall and sig as variables for the pricer
+    implied_vol_pricer = partial(BS_fwd_pricer, F = F, K = K, r=r, T=T)
+
+    # define the BS - p calculator
+    def diff_calculator(isCall, sig):
+        temp_price = implied_vol_pricer(isCall=isCall, sig=sig)
+        price_diff = temp_price - option_price
+        return price_diff
+
+    # initialize the value of the range of sig, and calculate the option price based on historical volatility
+    upper_bound = 1
+    lower_bound = 0.01
+    temp_sig = 0.5
+    price_diff = 1
+
+    # find two bounds of the root
+    while diff_calculator(isCall=isCall, sig=lower_bound) > 0:
+        lower_bound = lower_bound ** 2
+
+    upper_bound = lower_bound
+    while diff_calculator(isCall=isCall, sig=upper_bound) < 0:
+        upper_bound = upper_bound * 1.2
+
+    # bisection algo
+    while abs(price_diff) >= tol:
+        temp_sig = (lower_bound + upper_bound) / 2
+        price_diff = diff_calculator(isCall=isCall, sig=temp_sig)
+
+        if price_diff > 0:
+            upper_bound = temp_sig
+        else:
+            lower_bound = temp_sig
+
+    return temp_sig
+
+
+class BS_implied_vol():
+    def __init__(self, option_price):
+        self.option_price = option_price
+        self.today = pd.to_datetime('2023-09-01', format='%Y-%m-%d')
+    
+    def parity_implied_params(self, option_data, plot_parity = False):
+        # initialization
+        expiry_list = option_data['Expiry'].unique()
+        implied_params = pd.DataFrame(columns=['Expiry', 'implied_ir', 'implied_fwd'])
+        # today = pd.to_datetime('2023-09-01', format='%Y-%m-%d')
+
+        for expiry in expiry_list:
+            # for each expiry
+            option_price_expiry = self.option_price[self.option_price['Expiry'] == expiry]
+            option_price_expiry = option_price_expiry.dropna()
+            option_price_expiry['c-p'] = option_price_expiry['c'] - option_price_expiry['p']
+
+            # regress c-p on -strike
+            model = LinearRegression()
+            x = option_price_expiry[['Strike']]
+            y = option_price_expiry['c-p']
+            T = (pd.to_datetime(expiry, format='%Y-%m-%d') - self.today).days / 365
+            model.fit(x, y)
+            discount_factor = -model.coef_[0]
+            
+            # get implied risk free rate and implied forward price
+            forward_price = model.intercept_ / discount_factor
+            risk_free_rate = -np.log(discount_factor) / T
+
+            # format the output
+            implied_params = pd.concat([implied_params, pd.DataFrame({'Expiry': expiry, 'implied_ir': risk_free_rate, 'implied_fwd': forward_price}, index=[0])], axis=0)
+            
+            # plot c-p vs strike
+            if plot_parity:
+                plt.figure(figsize=(10, 6))
+                plt.scatter(option_price_expiry['Strike'], option_price_expiry['c-p'])
+                plt.plot(option_price_expiry['Strike'], model.predict(x), color='red')
+                plt.text(0.6, 0.8, f'c-p = {model.coef_[0]:.4f} * strike + {model.intercept_:.4f}', transform=plt.gca().transAxes)
+                plt.xlabel('Strike')
+                plt.ylabel('Call - Put')
+                title = 'Call - Put vs Strike for ' + expiry
+                plt.title(title)
+                plt.show()
+        
+        # reset index
+        implied_params = implied_params.reset_index(drop=True)
+
+        return implied_params
+
+    # plot implied ir vs expiry
+    def plot_ir(self, implied_params):
+        plt.figure(figsize=(10, 6))
+        plt.plot(implied_params['Expiry'], implied_params['implied_ir'])
+        plt.ylim(bottom=0)
+        plt.xticks(rotation=45)
+        plt.xlabel('Expiry')
+        plt.ylabel('Implied Interest Rate')
+        plt.title('Implied Interest Rate vs Expiry')
+        plt.show()
+    
+    # plot implied fwd vs expiry
+    def plot_fwd(self, implied_params):
+        plt.figure(figsize=(10, 6))
+        plt.plot(implied_params['Expiry'], implied_params['implied_fwd'])
+        plt.xticks(rotation=45)
+        plt.xlabel('Expiry')
+        plt.ylabel('Implied Forward Price')
+        plt.title('Implied Forward Price vs Expiry')
+        plt.show()
+
+    # calculate average implied interest rate
+    def get_ir(self, implied_params):
+        implied_ir_list = implied_params['implied_ir']
+        implied_ir_list = implied_ir_list[implied_ir_list > 0]
+        implied_ir = implied_ir_list.mean()
+
+        return implied_ir
+
+    def get_iv(self, option_data, implied_params, spx_data, r, plot_iv_scatter = False):
+        # initialization
+        expiry_list = option_data['Expiry'].unique()
+        implied_vol = pd.DataFrame(columns=expiry_list)
+
+        for expiry in expiry_list:
+            # for each expiry
+            option_price_expiry = self.option_price[self.option_price['Expiry'] == expiry]
+            option_strike = option_price_expiry['Strike'].unique()
+
+            for strike in option_strike:
+                # for each strike
+                option_price_strike = option_price_expiry[option_price_expiry['Strike'] == strike]
+
+                # check if call price is NaN
+                if not option_price_strike['c'].isna().values[0]:
+                    option_price_strike_k = option_price_strike['c'].values[0]
+                    isCall = True
+                if not option_price_strike['p'].isna().values[0]:
+                    option_price_strike_k = option_price_strike['p'].values[0]
+                    isCall = False
+
+                F = implied_params[implied_params['Expiry'] == expiry]['implied_fwd'].values[0]
+                K = strike
+                T = (pd.to_datetime(expiry, format='%Y-%m-%d') - self.today).days / 365
+
+                implied_vol_expiry_K = bisection_implied_vol(F, K, T, r, option_price_strike_k, isCall=isCall)
+                implied_vol.loc[strike, expiry] = implied_vol_expiry_K
+
+        implied_vol = implied_vol.sort_index()
+        implied_vol = implied_vol.transpose()
+        
+        # calculate forward moneyness
+        spx_0901 = spx_data.loc['2023-09-01', 'Open']
+        implied_vol.columns = implied_vol.columns.astype(float) / spx_0901
+    
+        # plot the rought implied volatility as scatter plot
+        if plot_iv_scatter:
+            plt.figure(figsize=(10, 6))
+            for strike in implied_vol.index:
+                plt.scatter(implied_vol.columns, implied_vol.loc[strike])
+            plt.xticks(rotation=45)
+            plt.xlabel('Strike')
+            plt.ylabel('Implied Volatility')
+            plt.title('Implied Volatility vs Expiry')
+            plt.legend(implied_vol.index)
+            plt.show()
+            
+
+        return implied_vol
+
+# plot implied volatility roughly
+# plt.figure(figsize=(10, 6))
+# for strike in implied_vol.index:
+#     plt.scatter(implied_vol.columns, implied_vol.loc[strike])
+# plt.xticks(rotation=45)
+# plt.xlabel('Strike')
+# plt.ylabel('Implied Volatility')
+# plt.title('Implied Volatility vs Expiry')
+# plt.show()
+
+
+
+# # calculate implied volatility
+# r = implied_ir
+# today = pd.to_datetime('2023-09-01', format='%Y-%m-%d')
+
+# # for each expiry
+# implied_vol = pd.DataFrame(columns=expiry_list_spx)
+# def iv_shortcut(expiry, strike, option_price_strike, iscall=True):
+#     F = implied_params[implied_params['Expiry'] == expiry]['implied_fwd'].values[0]
+#     K = strike
+#     T = (pd.to_datetime(expiry, format='%Y-%m-%d') - today).days / 365
+
+#     implied_vol = bisection_implied_vol(F, K, T, r, option_price_strike, isCall=iscall)
+#     return implied_vol
+
+# for expiry in expiry_list_spx:
+#     # for each expiry
+#     option_price_expiry = option_price[option_price['Expiry'] == expiry]
+#     option_strike = option_price_expiry['Strike'].unique()
+
+#     for strike in option_strike:
+#         # for each strike
+#         option_price_strike = option_price_expiry[option_price_expiry['Strike'] == strike]
+
+#         # check if call price is NaN
+#         if not option_price_strike['c'].isna().values[0]:
+#             option_price_strike_k = option_price_strike['c'].values[0]
+#             isCall = True
+#         if not option_price_strike['p'].isna().values[0]:
+#             option_price_strike_k = option_price_strike['p'].values[0]
+#             isCall = False
+ 
+#         F = implied_params[implied_params['Expiry'] == expiry]['implied_fwd'].values[0]
+#         K = strike
+#         T = (pd.to_datetime(expiry, format='%Y-%m-%d') - today).days / 365
+
+#         implied_vol_expiry_K = bisection_implied_vol(F, K, T, r, option_price_strike_k, isCall=isCall)
+#         implied_vol.loc[strike, expiry] = implied_vol_expiry_K
+
+# implied_vol = implied_vol.sort_index()
+# implied_vol = implied_vol.transpose()
+
+
+# ----------------------- test code -----------------------
+if __name__ == '__main__':
+    print('1')
